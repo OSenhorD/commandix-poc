5160b4e2022a6783b1a2c25a85fe221aec7378f3
David Marques <davidmarques.261299@gmail.com>
2026-09-06 15:21:10 -0300

    feat: Install tailwindcss

--- nexus-frontend/vite.config.ts
+++ nexus-frontend/vite.config.ts
@@ -1,14 +1,15 @@
-import path from 'node:path';
-import react, { reactCompilerPreset } from '@vitejs/plugin-react';
-import babel from '@rolldown/plugin-babel';
-import { defineConfig } from 'vite';
+import path from "node:path";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
   resolve: {
     alias: {
-      '@': path.resolve(import.meta.dirname, './src'),
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
 });
